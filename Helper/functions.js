62712bb6f4ca8f05a52e0cc68339f6cfa0711713
ArrowSK <56678503+ArrowSK@users.noreply.github.com>
2023-02-14 11:22:34 +0100

    update model Curie

--- Helper/functions.js
+++ Helper/functions.js
@@ -28,6 +28,9 @@ const getChat = async (text) => {
       model: "text-curie-001",
       prompt: text,
       temperature: 0.4,
+      top_p: 0.3,
+      frequency_penalty: 0,
+      presence_penalty: 0,
       max_tokens: 1000,
     });
 
