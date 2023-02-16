ed21d8cfd690e021e9f85d23910356b2ebb81684
ArrowSK <56678503+ArrowSK@users.noreply.github.com>
2023-02-16 15:14:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -89,8 +89,8 @@ bot.command("know", async (ctx) => {
   if (text) {
     ctx.sendChatAction("typing");
     const searchResult = await googleSearch(text);
-	const trimmedResult = searchResult.substring(0, 500);
-    const prompt = searchResult ? `${text} || " This is waht I know from the internet about it, but please summarize: " ||  ${trimmedResult}` : text;
+	const trimmedResult = searchResult.substring(0, 1500);
+    const prompt = searchResult ? `${text} || " This is waht I know from the internet about it, but please summarize it for me: " ||  ${trimmedResult}` : text;
     const res = await getChat(prompt);
 	const trimres = res.substring(0, 3500);
     if (trimres) {
