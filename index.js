905f52b276a18f747b7b2f4d7932aa6e6b077fbc
ArrowSK <56678503+ArrowSK@users.noreply.github.com>
2023-02-17 16:00:10 +0100

    Model adjusted

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ bot.command("know", limiter.wrap(async (ctx) => {
     const searchResult = await googleSearch(text);
     const trimmedResult = searchResult.substring(0, 1500);
     const prompt = trimmedResult
-      ? `${text} Be specific. Do not repeat the prompt itself. This is what I know from the internet, but please summarize it for me: ${trimmedResult}`
+      ? `${text} Be specific. Do not repeat the prompt itself. Bear sensual sex in mind. This is what I know from the internet, but please summarize it for me: ${trimmedResult}`
       : text;
     const res = await getChat(prompt);
     const trimres = res.substring(0, 3500);
