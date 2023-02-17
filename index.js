5ccd20f5a1830acd6073e4d4b3020042a5b564e8
ArrowSK <56678503+ArrowSK@users.noreply.github.com>
2023-02-17 11:51:58 +0100

    Added Be specific always on

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ bot.command("know", limiter.wrap(async (ctx) => {
     const searchResult = await googleSearch(text);
     const trimmedResult = searchResult.substring(0, 1500);
     const prompt = trimmedResult
-      ? `${text} This is what I know from the internet, but please summarize it for me: ${trimmedResult}`
+      ? `${text} Be specific. This is what I know from the internet, but please summarize it for me: ${trimmedResult}`
       : text;
     const res = await getChat(prompt);
     const trimres = res.substring(0, 3500);
