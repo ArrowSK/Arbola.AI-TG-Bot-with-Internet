348b08a7ff8278ee9a6391400e667a6bfd630641
ArrowSK <56678503+ArrowSK@users.noreply.github.com>
2023-03-09 14:39:08 +0100

    Update functions.js

--- Helper/functions.js
+++ Helper/functions.js
@@ -29,7 +29,6 @@ const getChat = async (text) => {
   messages: [{role: "user", content: text}],
       max_tokens: 750,
 		temperature: 0.3,
-		      top_p: 0.5,
       frequency_penalty: 0.2,
       presence_penalty: 0.05,
 });
