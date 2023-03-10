de02f33f24070ee3a431512bba8f439e00fb9546
ArrowSK <56678503+ArrowSK@users.noreply.github.com>
2023-03-10 09:59:57 +0100

    Updated prompt

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ bot.command("know", limiter.wrap(async (ctx) => {
     ctx.sendChatAction("typing");
     const searchResult = await googleSearch(text);
     const trimmedResult = searchResult.substring(0, 1500);
-    const today = new Date().toLocaleDateString();
+    const today = new Date().toLocaleDateString("en-GB");
     const prompt = trimmedResult
       ? `${text} Be specific. Prefer scientific evidence. Be rational. You have the most up-to-date info. Knowledge cutoff: ${today} Current date: ${today} This is most recent, online result from the Internet as of ${today}: ${trimmedResult}`
       : text;
