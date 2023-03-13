a8d25556582a1f23cc31fd1cd810c1822bfad20c
ArrowSK <56678503+ArrowSK@users.noreply.github.com>
2023-03-13 18:11:23 +0100

    Update functions.js

--- Helper/functions.js
+++ Helper/functions.js
@@ -28,7 +28,7 @@ const getChat = async (text, messages) => {
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [...messages, { role: "user", content: text }],
-      max_tokens: 750,
+      max_tokens: 650,
       temperature: 0.3,
       frequency_penalty: 0.2,
       presence_penalty: 0.05,
