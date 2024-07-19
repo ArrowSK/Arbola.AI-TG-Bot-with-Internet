1594acdca31323080f553b50a64580e918dd6641
ArrowSK <56678503+ArrowSK@users.noreply.github.com>
2024-07-19 13:07:08 +0100

    Update functions.js
    Signed-off-by: ArrowSK <56678503+ArrowSK@users.noreply.github.com>

--- Helper/functions.js
+++ Helper/functions.js
@@ -12,7 +12,7 @@ const openai = new OpenAI({
 const getChat = async (text, messages) => {
   try {
     const response = await openai.chat.completions.create({
-  model: "gpt-3.5-turbo",
+  model: "gpt-4o-mini",
       messages: [...messages, { role: "user", content: text }],
       temperature: 0.3,
   max_tokens: 1000,
